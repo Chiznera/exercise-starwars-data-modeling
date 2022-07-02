e843ab4419d6b296edde656a4763d3828ed3b441
Chiznera <chiznera@gmail.com>
2022-07-02 00:54:42 +0000

    missing data types and relationships

--- src/models.py
+++ src/models.py
@@ -8,23 +8,57 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(256), nullable=False)
+    first_name = Column(String(256), nullable=False)
+    last_name = Column(String(256), nullable=False)
+    email = Column(String(256), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(256), nullable=False)
+    planet_id = Column(String(256), nullable=False)
+    character_id = Column(String(256), nullable=False)
+
+
+class Character(Base):
+    __tablename__ = 'charcter'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(256))
+    birth_year = Column(String(256))
+    eye_color = Column(String(256), nullable=False)
+    gender = Column(String(256), nullable=False)
+    hair_color = Column(String(256), nullable=False)
+    height = Column(String(256), nullable=False)
+    mass = Column(String(256), nullable=False)
+    skin_color = Column(String(256), nullable=False)
+    homeworld = Column(String(256), nullable=False)
+    url = Column(String(256), nullable=False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+    diameter = Column(String(256))
+    rotation_period = Column(String(256), nullable=False)
+    orbital_period = Column(Integer, ForeignKey('person.id'))
+    gravity = Column(String(256), nullable=False)
+    population = Column(String(256), nullable=False)
+    climate = Column(String(256), nullable=False)
+    terrain = Column(String(256), nullable=False)
+    surface_water = Column(String(256), nullable=False)
+    url = Column(String(256), nullable=False)
 
     def to_dict(self):
         return {}
